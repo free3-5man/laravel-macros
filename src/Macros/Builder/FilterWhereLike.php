273d48fb85aae6b45422e67aab5f4def041ec4fe
Michael Freeman <freeman3_5man@163.com>
2023-02-17 16:26:07 +0800

    fix bug

--- src/Macros/Builder/FilterWhereLike.php
+++ src/Macros/Builder/FilterWhereLike.php
@@ -22,9 +22,8 @@ class FilterWhereLike
                     // zh-cn: 如果是整数，说明是自然的数组index，则表字段和请求字段同一
                     $tableField = is_int($tableField) ? $requestField : $tableField;
 
-                    // use ?field=&... to represent whereNull(field)
                     /** @var Builder $this */
-                    $this->when(array_key_exists($requestField, $data) && is_null($data[$requestField]), function ($query) use ($table, $tableField, $data, $requestField) {
+                    $this->when(!empty($data[$requestField]), function ($query) use ($data, $tableField, $requestField, $table) {
                         return $query->where("{$table}.{$tableField}", 'ilike', '%' . $data[$requestField] . '%');
                     });
                 }
