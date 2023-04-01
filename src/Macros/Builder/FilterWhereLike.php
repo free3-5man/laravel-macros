a2826b5228bee5ab6ceb88b3928d43e2720fbe02
Michael Freeman <freeman3_5man@163.com>
2023-04-01 15:32:40 +0800

    fixbug:支持多个表格之间是 or 查询

--- src/Macros/Builder/FilterWhereLike.php
+++ src/Macros/Builder/FilterWhereLike.php
@@ -18,18 +18,24 @@ class FilterWhereLike
 
             /** @var Builder $this */
             return $this->where(function ($query) use ($whereFields, $data) {
-                // 多个 $table 之间是 or 的关系
+                $tablesCount = count(array_keys($whereFields));
+                $fnBuildOrQuery = function (Builder $query, array $assoc, string $table, string $clue) {
+                    foreach ($assoc as $tableField => $requestField) {
+                        $tableField = is_int($tableField) ? $requestField : $tableField;
+                        $query->orWhere("{$table}.{$tableField}", 'ilike', "%{$clue}%");
+                    }
+                };
+
                 foreach ($whereFields as $table => $fields) {
-                    foreach (collect($fields)->groupBy(fn ($item) => $item, $preserveKeys = true) as $requestField => $assoc) {
+                    foreach (collect($fields)->groupBy(fn ($item) => $item, $preserveKeys = true)->toArray() as $requestField => $assoc) {
                         if (isset($data[$requestField])) {
                             $clue = $data[$requestField];
-                            /** @var Builder $this */
-                            $query->orWhere(function ($query) use ($assoc, $table, $clue) {
-                                foreach ($assoc as $tableField => $requestField) {
-                                    $tableField = is_int($tableField) ? $requestField : $tableField;
-                                    $query->orWhere("{$table}.{$tableField}", 'ilike', "%{$clue}%");
-                                }
-                            });
+                            if ($tablesCount > 1)
+                                $query->orWhere(function ($query) use ($fnBuildOrQuery, $assoc, $table, $clue) {
+                                    $fnBuildOrQuery($query, $assoc, $table, $clue);
+                                });
+                            else
+                                $fnBuildOrQuery($query, $assoc, $table, $clue);
                         }
                     }
                 }
